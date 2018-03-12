1d0af65cbed7e4e434e01c005ce8bc2216dd9a73
David Kröll <david-kroell@users.noreply.github.com>
2018-03-12 10:25:24 +0100

    add distro detector, still wip

--- wordpress.sh
+++ wordpress.sh
@@ -2,6 +2,40 @@
 #by Chrisitan Gubesch
 #last updated 3/7/2018
 
+function detect_distro {
+    if [ -f /etc/os-release ]; then
+        # freedesktop.org and systemd
+        . /etc/os-release
+        OS=$NAME
+        VER=$VERSION_ID
+    elif type lsb_release >/dev/null 2>&1; then
+        # linuxbase.org
+        OS=$(lsb_release -si)
+        VER=$(lsb_release -sr)
+    elif [ -f /etc/lsb-release ]; then
+        # For some versions of Debian/Ubuntu without lsb_release command
+        . /etc/lsb-release
+        OS=$DISTRIB_ID
+        VER=$DISTRIB_RELEASE
+    elif [ -f /etc/debian_version ]; then
+        # Older Debian/Ubuntu/etc.
+        OS=Debian
+        VER=$(cat /etc/debian_version)
+    elif [ -f /etc/SuSe-release ]; then
+        # Older SuSE/etc.
+        ...
+    elif [ -f /etc/redhat-release ]; then
+        # Older Red Hat, CentOS, etc.
+        ...
+    else
+        # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+        OS=$(uname -s)
+        VER=$(uname -r)
+    fi
+
+    return $OS $VER
+}
+
 function install_wordpress {
     cd "${1}"
     wget https://wordpress.org/latest.tar.gz
@@ -32,8 +66,17 @@ function install_wordpress {
 
 
 echo "Welcome to wordpress installation!"
-echo -n "Please enter your wished installation destination: "
-read destination
+echo "Detecting operation system..."
+DISTRO=detect_distro
+read -p "Are you currently on "$DISTRO"? [Y/n]: " decision
+if [ "${decision}" == "y" ] || [ "${decision}" == "Y" ] || [ "${decision}" == "" ] ; then
+    echo ""
+else
+    echo -e "\nclosing script"
+    exit 1
+fi
+    
+read -p "Please enter your wished installation destination: " destination
 echo ""
 
 if [ -d ${destination} ] ; then
